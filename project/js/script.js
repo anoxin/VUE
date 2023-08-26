a30ed089e4b6b7d6b1cc5afbf303b9a7f70c128f
Anton Anokhin <anoxin.toscha1@yandex.ru>
2023-08-26 12:50:32 +0300

    create: lesson2 and project

---
+++ project/js/script.js
@@ -0,0 +1,34 @@
+new Vue({
+    el: '#article-news-card',
+
+    data: {
+        carts: [
+            {
+                id: 1, name: "Interior Design", img: "./img/cart-design/1.jpg", text: "Best For Any Office & Business Interior Solution", data: "25 December,2022"
+            },
+            {
+                id: 2, name: "Interior Design", img: "./img/cart-design/2.jpg", text: "Best For Any Office & Business Interior Solution", data: "25 December,2022"
+            },
+            {
+                id: 3, name: "Interior Design", img: "./img/cart-design/3.jpg", text: "Best For Any Office & Business Interior Solution", data: "25 December,2022"
+            },
+            {
+                id: 4, name: "Interior Design", img: "./img/cart-design/1.jpg", text: "Best For Any Office & Business Interior Solution", data: "25 December,2022"
+            },
+            {
+                id: 5, name: "Interior Design", img: "./img/cart-design/2.jpg", text: "Best For Any Office & Business Interior Solution", data: "25 December,2022"
+            },
+            {
+                id: 6, name: "Interior Design", img: "./img/cart-design/3.jpg", text: "Best For Any Office & Business Interior Solution", data: "25 December,2022"
+            },
+
+        ],
+
+
+
+    },
+
+    methods: {
+
+    }
+})
